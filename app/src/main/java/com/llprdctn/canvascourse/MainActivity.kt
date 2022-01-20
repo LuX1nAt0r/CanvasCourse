ecfd9f47852c38bd1a670070ebd7eec604bdc69a
LuX1nAt0r <66088547+LuX1nAt0r@users.noreply.github.com>
2022-01-20 17:07:31 +0100

    First Course

---
+++ app/src/main/java/com/llprdctn/canvascourse/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.llprdctn.canvascourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.llprdctn.canvascourse.ui.theme.CanvasCourseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCanvas()
+        }
+    }
+}
+
+
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+        ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(100f, 100f),
+            size = Size(100F, 100F),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                center = center,
+                radius = 100f
+            ),
+            radius = 100f,
+
+        )
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = true,
+            topLeft = Offset(100f, 500f),
+            size = Size(200f, 200f)
+        )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f, 100f),
+            size = Size(200f, 300f)
+        )
+
+    }
+}
