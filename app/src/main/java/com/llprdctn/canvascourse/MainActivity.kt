b9e55fdbeff53cf0b8311ffe0f6ad0477fffa0d1
LuX1nAt0r <66088547+LuX1nAt0r@users.noreply.github.com>
2022-01-22 20:02:14 +0100

    Tested somethings

--- app/src/main/java/com/llprdctn/canvascourse/MainActivity.kt
+++ app/src/main/java/com/llprdctn/canvascourse/MainActivity.kt
@@ -2,9 +2,12 @@ package com.llprdctn.canvascourse
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -13,8 +16,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
@@ -28,140 +36,84 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            Animation()
         }
     }
 }
 
-
+@Preview
 @Composable
-fun MainScreen() {
-    var points by remember {
-        mutableStateOf(0)
-    }
+fun Animation() {
 
-    var isTimerRunning by remember {
-        mutableStateOf(false)
-    }
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Points $points",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Button(onClick = {
-                isTimerRunning = !isTimerRunning
-                points = 0
-            }) {
-                Text(text = if (isTimerRunning)"Reset" else "Start")
-            }
-            CountDownTimer(
-                isTimerRunning = isTimerRunning
-            ) {
-                isTimerRunning = false
-            }
-
-        }
-        BallClicker(
-            enabled = isTimerRunning
-        ) {
-            points++
-        }
-
-    }
-}
-
-
-@Composable
-fun CountDownTimer(
-    time: Int = 30000,
-    isTimerRunning: Boolean = false,
-    onTimerEnd: () -> Unit = {}
-) {
-    var curTime by remember {
-        mutableStateOf(time)
-    }
-    LaunchedEffect(key1 = curTime, key2 = isTimerRunning) {
-        if(!isTimerRunning) {
-            curTime = time
-            return@LaunchedEffect
-        }
-        if (curTime > 0) {
-            delay(1000L)
-            curTime -= 1000
-        } else {
-            onTimerEnd()
-        }
-    }
-    Text(
-        text = (curTime / 1000).toString(),
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold
-    )
-}
-
-
-@Composable
-fun BallClicker(
-    radius: Float = 100f,
-    enabled: Boolean = false,
-    ballColor: Color = Color.Red,
-    onBallClick: () -> Unit = {}
-) {
     BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
-        var ballPosition by remember {
-            mutableStateOf(
-                randomOffset(
-                    radius = radius,
-                    width = constraints.maxWidth,
-                    height = constraints.maxHeight
-                )
+
+
+        val deltaXAnim = rememberInfiniteTransition()
+        val dx by deltaXAnim.animateFloat(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(500, easing = LinearEasing)
             )
+        )
+
+        val screenWidthPx = with(LocalDensity.current) {
+            (LocalConfiguration.current.screenHeightDp * density)
         }
+        val animTranslate by animateFloatAsState(
+            targetValue = screenWidthPx,
+            animationSpec = TweenSpec(10000, easing = LinearEasing)
+        )
+
+        val waveHeight by animateFloatAsState(
+            targetValue = 0f,
+            animationSpec = TweenSpec(10000, easing = LinearEasing)
+        )
+
+        val animColor by animateColorAsState(
+            targetValue = if (init) green200 else red700,
+            animationSpec = TweenSpec(if (init) 0 else timeSpec.toInt(), easing = LinearEasing)
+        )
+
+        val waveWidth = 200
+        val originalY = 150f
+        val path = Path()
+
+
+
         Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .pointerInput(enabled) {
-                    if (!enabled) {
-                        return@pointerInput
-                    }
-                    detectTapGestures {
-                        val distance = sqrt(
-                            (it.x - ballPosition.x).pow(2) +
-                                    (it.y - ballPosition.y).pow(2)
+            modifier = Modifier.fillMaxSize(),
+            onDraw = {
+                translate(top = animTranslate) {
+                    drawPath(path = path, color = animColor)
+                    path.reset()
+                    val halfWaveWidth = waveWidth / 2
+                    path.moveTo(-waveWidth + (waveWidth * dx), originalY.dp.toPx())
+
+                    for (i in -waveWidth..(size.width.toInt() + waveWidth) step waveWidth) {
+                        path.relativeQuadraticBezierTo(
+                            halfWaveWidth.toFloat() / 2,
+                            -waveHeight,
+                            halfWaveWidth.toFloat(),
+                            0f
                         )
-                        if (distance <= radius) {
-                            ballPosition = randomOffset(
-                                radius = radius,
-                                width = constraints.maxWidth,
-                                height = constraints.maxHeight
-                            )
-                            onBallClick()
-                        }
+                        path.relativeQuadraticBezierTo(
+                            halfWaveWidth.toFloat() / 2,
+                            waveHeight,
+                            halfWaveWidth.toFloat(),
+                            0f
+                        )
                     }
+
+                    path.lineTo(size.width, size.height)
+                    path.lineTo(0f, size.height)
+                    path.close()
                 }
-        ) {
-            drawCircle(
-                color = ballColor,
-                radius = radius,
-                center = ballPosition
-            )
-        }
+            }
+        )
+
     }
 
-}
 
 
-private fun randomOffset(radius: Float, width: Int, height: Int): Offset {
-    return Offset(
-        x = Random.nextInt(radius.roundToInt(), width - radius.roundToInt()).toFloat(),
-        y = Random.nextInt(radius.roundToInt(), height - radius.roundToInt()).toFloat()
-    )
 }
